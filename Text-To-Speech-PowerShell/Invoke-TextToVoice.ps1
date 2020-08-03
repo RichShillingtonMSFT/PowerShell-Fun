8887968b22ea270d9b739e72fa1840d47306d588
Rich Shillington <rishilli@microsoft.com>
2020-08-03 09:29:25 -0400

    Add Text to Speech Script

---
+++ Text-To-Speech-PowerShell/Invoke-TextToVoice.ps1
@@ -0,0 +1,16 @@
+﻿# For more information see this link
+# https://docs.microsoft.com/en-us/dotnet/api/system.speech.synthesis?view=netframework-4.8
+
+# Give this a path to a text file with the words you want PowerShell to read
+$Words = Get-Content 'C:\VoiceTest.txt'
+
+# This adds the speech assembly
+Add-Type -AssemblyName System.Speech
+$SpeechSynthesizer = New-Object System.Speech.Synthesis.SpeechSynthesizer
+
+# This prompts you to select a voice
+$Voice = $SpeechSynthesizer.GetInstalledVoices().VoiceInfo | Out-GridView -Title "Please Select a voice." -PassThru
+$SpeechSynthesizer.SelectVoice("$($Voice.Name)")
+
+# This part reads the words from the file
+$SpeechSynthesizer.Speak("$Words")
